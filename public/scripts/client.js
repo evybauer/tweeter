c84a0bddb205c9627667e902e1e07853ce1fb496
Evelyn Bauer <evybauer@gmail.com>
2019-12-19 03:47:55 +0000

    Add form submission and loadTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,42 +4,78 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+$(document).ready(function() {
+  let $form = $('.new-tweet-form');
+  // console.log('beta', $form.serialize())
+    $form.on('submit', function (event) {
+      event.preventDefault();
+        let input = $(this).find('textarea').val();
+          if (input.length > 140) {
+            alert("More than 140");
+            return;
+          }
+          if (input === '' || input === null) {
+            alert("Please, tweet something");
+            return;
+          }
+      console.log('Button clicked, performing ajax call...');
+      let data = $form.serialize();
+      console.log(data);
+      
+      return $.ajax('/tweets', { method: 'POST', data: $form.serialize()},)
+      .then(function () {
+        loadTweets () 
+        console.log('Success!');
+      })
+    });
+  });
 
 const renderTweets = function(tweets) {
   // console.log("All tweets", tweets);
   // TODO: delete all old tweets
+  $('.tweets-container').empty();
   for (let post of tweets) {
     // console.log("tweet: ", post);
     const $tweet = createTweetElement(post);
-    $('.tweets-container').append($tweet);
+    $('.tweets-container').prepend($tweet);
   }
 }
+
+const loadTweets = function () {
+  $.ajax({
+    url:"/tweets"
+
+  }).then(renderTweets)
+}
+
+loadTweets();
+
+
 
 const createTweetElement = function(tweet) {
   let $tweet = $('<article>').addClass('tweet');
@@ -56,7 +92,7 @@ const createTweetElement = function(tweet) {
     
     <footer class = "footer">    
     <div class="time">
-      <span class="createdAt"> 
+      <span class="createdAt">
       ${tweet.created_at} 
       </span>
     </div>
@@ -69,31 +105,5 @@ const createTweetElement = function(tweet) {
   );
   return $tweet
 }
-
-$(document).ready(function() {
-  renderTweets(data);
-});
-
-
-$(document).ready(function() {
-  let $form = $('.new-tweet-form');
-  // console.log('beta', $form.serialize())
-  $form.on('submit', function (event) {
-    event.preventDefault();
-    console.log('Button clicked, performing ajax call...');
-    let data = $form.serialize();
-    console.log(data);
-
-    return $.ajax('/tweets', { method: 'POST', data: $form.serialize()})
-    
-    .then(function (data) {
-      console.log('Success!');
-    })
-  });
-});
-
-
-
-
 
 
