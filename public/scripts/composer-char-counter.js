b8678ba4ebbceb1bca4a57fc8c395ae2f0cde8ba
Evelyn Bauer <evybauer@gmail.com>
2019-12-19 03:51:19 +0000

    Check if conditionals still work after adding the text textarea conditions to the client.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,9 +7,9 @@ $('textarea').keyup(function() {
   let maxLength = 140;
   let length = $(this).val().length;
   let totalLength = maxLength - length;
-  if (totalLength <= 0) {
+  
+    if(totalLength <= 0) {
     $(this).siblings("span").text(totalLength).addClass('exceeded');
-
   } else {
     $(this).siblings("span").text(totalLength).removeClass('exceeded');
   }
